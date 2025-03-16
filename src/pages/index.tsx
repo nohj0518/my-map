3ca778c29e1cdb6e99e9b11b61e436cbad1bccc0
nohj0518 <nohj0518@daum.net>
2025-03-16 23:08:38 +0900

    Markers feature and ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 
 import Script from "next/script";
 import Map from "@/components/Map";
+import Markers from "@/components/Markers";
+
+import * as stores from "@/data/store_data.json";
+import { useState } from "react";
+import StoreBox from "@/components/StoreBox";
 
 declare global {
   interface Window {
@@ -11,9 +16,18 @@ declare global {
 }
 
 export default function Home() {
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  const storeDatas = stores["DATA"];
   return (
     <>
-      <Map />
+      <Map setMap={setMap} />
+      <Markers
+        storeDatas={storeDatas}
+        map={map}
+        setCurrentStore={setCurrentStore}
+      />
+      <StoreBox store={currentStore} setStore={setCurrentStore} />
     </>
   );
 }
